6d57ec44febe540e784fa953496678a18b2d96a4
ianfield@live.com <ianfield@live.com>
2014-03-11 22:21:26 -0700

    automation please

--- src/html.tpl.php
+++ src/html.tpl.php
@@ -3,7 +3,7 @@
 /**
  * @file
  * Default theme implementation to display the basic html structure of a single
- * Drupal page.
+ * Drupal page. test automation
  *
  * Variables:
  * - $css: An array of CSS files for the current page.
